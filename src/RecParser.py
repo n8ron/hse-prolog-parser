f42684cedc7d9ea4a1348f579b91ab81173a2aca
NikitaAbramov <nik_abramov22@mail.ru>
2020-10-01 00:59:59 +0300

    Add Lexer and Parser

---
+++ src/RecParser.py
@@ -0,0 +1,130 @@
+from lex import *
+import sys
+
+
+class Node:
+    def __init__(self, left, right, name):
+        self.left = left
+        self.right = right
+        self.name = name
+
+
+class Parser:
+    def __init__(self, s):
+        self.lex = lexer(s)
+        self.current = next(self.lex)
+        self.prev_lineno = 1
+        self.prev_lexpos = 0
+
+    def accept(self, c):
+        if self.current == '\0':
+            return False
+        if self.current.type == c:
+            self.prev_lineno = self.current.lineno
+            self.prev_lexpos = self.current.lexpos + len(self.current.value)
+            self.current = next(self.lex)
+            return True
+        return False
+
+    def log_err(self, tok, expected):
+        if tok == '\0':
+            print("Expected", expected, "but got", "NOTHING", "at line",
+                  self.prev_lineno, "col", self.prev_lexpos)
+            raise ValueError()
+        print("Expected", expected, "but got", tok.type, "at line",
+              tok.lineno, "col", tok.lexpos)
+        raise ValueError()
+
+    def id(self):
+        l = self.current
+        if self.accept('ID'):
+            return Node(None, None, l.value)
+        self.log_err(l, 'ID')
+        return None
+
+    def disj(self):
+        l = self.conj()
+        if self.accept('OR'):
+            r = self.disj()
+            if r is None:
+                l.lexpos += len(l.value)
+                self.log_err(l, 'expression')
+                return None
+            return Node(l, r, "or")
+        return l
+
+    def conj(self):
+        l = self.expr()
+        if self.accept('AND'):
+            r = self.conj()
+            if r is None:
+                l.lexpos += len(l.value)
+                self.log_err(l, 'expression')
+                return None
+            return Node(l, r, "and")
+        return l
+
+    def expr(self):
+        if self.accept('OBR'):
+            r = self.disj()
+            if self.accept('CBR'):
+                return r
+            self.log_err(self.current, 'CBR')
+            return None
+        return self.id()
+
+    def build(self):
+        l = self.id()
+        if self.accept('OP'):
+            r = self.disj()
+            if r is not None:
+                return Node(l, r, ":-")
+        return l
+
+
+def pr(node):
+    a = "("
+    if node.left is not None:
+        a += pr(node.left)
+    a += " " + node.name + " "
+    if node.right is not None:
+        a += pr(node.right)
+    a += ')'
+    return a
+
+
+def lexer(s):
+    for c in s:
+        yield c
+    while True:
+        yield '\0'
+
+
+def isCorrect():
+    no_errors, tokens = get_tokes(sys.argv[1])
+    expression = []
+    if no_errors:
+        for token in tokens:
+            if token.type == 'POINT':
+                try:
+                    p = Parser(expression)
+                    tree = p.build()
+                    if tree is not None:
+                        print(pr(tree))
+                except ValueError:
+                    print("Failed to parse")
+                    return False
+                expression = []
+            else:
+                expression.append(token)
+    if len(expression) > 0:
+        print("Expected POINT but got NOTHING at line",
+              tokens[-1].lineno, "col", tokens[-1].lexpos + len(tokens[-1].value))
+        print("Failed to parse")
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    if isCorrect():
+        print("Correct file")
---
+++ src/lex.py
@@ -0,0 +1,95 @@
+import ply.lex as lex
+import sys
+
+
+class LineCounter:
+    def __init__(self):
+        self.prev_line = 1
+        self.prev_tok = 0
+        self.last_line_tok = 0
+        self.col = 0
+        self.no_error = True
+
+    def update_pos(self, token):
+        if token.lineno != self.prev_line:
+            self.prev_line = token.lineno
+            self.last_line_tok = self.prev_tok
+        self.prev_tok = token.lexpos + len(str(token.value))
+        token.lexpos -= self.last_line_tok
+
+
+counter = LineCounter()
+
+reserved = {
+    'module': 'MODULE',
+    'sig': 'SIG',
+    'type': 'TYPE'
+}
+
+tokens = [
+             'ID',
+             'OP',
+             'OR',
+             'AND',
+             'OBR',
+             'CBR',
+             'POINT'
+         ] + list(reserved.values())
+
+t_OP = r':-'
+
+t_OBR = r'\('
+t_CBR = r'\)'
+
+t_AND = r'\,'
+t_OR = r'\;'
+t_POINT = r'\.'
+
+
+def t_ID(t):
+    r"""[a-zA-Z_][a-zA-Z_0-9]*"""
+    t.type = reserved.get(t.value, 'ID')
+    return t
+
+
+t_ignore = ' \t'
+
+
+def t_newline(t):
+    r"""\n+"""
+    t.lexer.lineno += len(t.value[0])
+    counter.update_pos(t)
+
+
+def t_error(t):
+    counter.update_pos(t)
+    counter.no_error = False
+    print("Illegal character '%s' at line %i at pos %i" % (t.value[0], t.lineno, t.lexpos))
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
+
+
+def get_tokes(filename):
+    file_tokens = []
+    with open(filename) as f:
+        lexer.input(f.read())
+        while True:
+            tok = lexer.token()
+            if not tok:
+                break
+            counter.update_pos(tok)
+            file_tokens.append(tok)
+    return counter.no_error, file_tokens
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as mf:
+        lexer.input(mf.read())
+        while True:
+            mtok = lexer.token()
+            if not mtok:
+                break
+            counter.update_pos(mtok)
+            print(mtok)
